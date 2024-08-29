d8a13ee7aed7a095e94d404d06c13d1e4342cf82
Dev Marker <fistondev@tuta.io>
2024-08-29 04:12:26 +0200

    mine

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import { BlogPosts } from "app/components/posts";
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Fiston 🍀
+      </h1>
+      <p className="mb-4">
+        {`Software developer with a knack for solving problems. 
+          I write clean code and bad jokes. 
+        Always learning, rarely napping. 
+        Let's build something cool together.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  );
+}
